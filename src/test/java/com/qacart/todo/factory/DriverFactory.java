c33e198919f2a477db04b809be986ddb54552e44
OmarAlaa404 <omar97alaa.qa@gmail.com>
2024-08-20 22:09:26 +0100

    first committttttttttt

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -2,14 +2,50 @@ package com.qacart.todo.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
 public class DriverFactory {
+
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public enum BrowserType {
+        CHROME,
+        FIREFOX,
+        EDGE
+    }
+
     public WebDriver initializeDriver() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.manage().window().maximize();
-        return driver;
+        // Fetch browser type from system property, default to CHROME if not set
+        String browser = System.getProperty("browser", "CHROME").toUpperCase();
+
+        return initializeDriver(BrowserType.valueOf(browser));
+    }
+
+    public WebDriver initializeDriver(BrowserType browser) {
+        WebDriver webDriver;
+
+        switch (browser) {
+            case CHROME:
+                webDriver = new ChromeDriver();
+                break;
+            case FIREFOX:
+                webDriver = new FirefoxDriver();
+                break;
+            case EDGE:
+                webDriver = new EdgeDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Browser type not supported: " + browser);
+        }
+
+        driver.set(webDriver);
+
+        driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get().manage().window().maximize();
+
+        return driver.get();
     }
 }
--- src/test/java/com/qacart/todo/testcases/DeleteTodoTest.java
+++ src/test/java/com/qacart/todo/testcases/DeleteTodoTest.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DeleteTodoTest extends BaseTest {
-    @Test
+    @Test (enabled = false)
     public void ShouldBeAbleToAddNewTodo() {
         LoginPage loginPage = new LoginPage(driver);
         boolean isNoTodoDisplayed = loginPage
